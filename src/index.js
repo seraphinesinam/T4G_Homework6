3a6cf1aaaece18a0227db6b980f20a7814a6266f
unknown <esinamseraphine7@gmail>
2024-11-29 04:34:25 -0800

    added homework6

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let name="Seraphine";
+console.log(name);
+let age =21;
+console.log(age);
+let carrot=6+9;
+console.log(carrot);
+let cucumber=31-5;
+
+
+console.log(cucumber);
+
+let lettuce=14/4;
+
+console.log(lettuce);
+
+let tomatoes =7*9;
+
+console.log(tomatoes);
+
+let yam = 15%4;
+
+console.log(yam);
+
+let potatoes= true;//boolean
+
+console.log(typeof{potatoes});
+
+var number=prompt("how many potatoes do you want");
+
+
+console.log(number+"potatoes");
+
+let pepper=prompt("i like potatoes");
+
+console.log(potatoes);
+
+
+
+
+
+
+
+
+
+
